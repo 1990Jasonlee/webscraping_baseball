74ee68b1ccfd48c3d3c715558fc45ebbf603a190
Jason <leejason1990@gmail.com>
2022-05-20 21:06:15 -0400

    update

--- baseball.py
+++ baseball.py
@@ -5,6 +5,15 @@ import streamlit as st
 st.title('Baseball Stats')
 #using https://www.baseball-reference.com/leagues/majors/2021-standard-batting.shtml
 
+st.sidebar.header('User Input Feature')
+select_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2022))))
+
 @st.cache
 def load_data(year):
     url = f'https://www.baseball-reference.com/leagues/majors/{year}-standard-batting.shtml'
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    df = df.iloc[:-3]
+    return df
+df = load_data(select_year)
+
