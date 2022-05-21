5948b5c27341c9a261cbab2bd5970f6d8d9e5038
Jason <leejason1990@gmail.com>
2022-05-20 20:34:17 -0400

    update

--- baseball.py
+++ baseball.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 
+st.title('Baseball Stats')
+#using https://www.baseball-reference.com/leagues/majors/2021-standard-batting.shtml
+
+@st.cache
+def load_data(year):
+    url = f'https://www.baseball-reference.com/leagues/majors/{year}-standard-batting.shtml'
